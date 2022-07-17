0e71678cbfbf25697e7d04a2778bd99dc59d2278
Sauuudi <iliasbox2@gmail.com>
2022-07-17 02:26:14 +0200

    falat info pokemon y estilo

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../services/data.service';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css'],
+  animations: [
+    // Each unique animation requires its own trigger. The first argument of the trigger function is the name
+    trigger('rotatedState', [
+      state('default', style({ transform: 'rotate(0 )' })),
+      state('rotated', style({ transform: 'rotate(-360deg)' })),
+      transition('rotated => default', animate('0.4s')),
+      transition('default => rotated', animate('0.4s'))
+    ])
+]
 })
 export class MainPageComponent implements OnInit {
   pokemons: any[] = [];
   cardTable: boolean = true;
+  state: string = 'default';
 
   constructor(private dataService: DataService) {}
 
@@ -30,4 +41,7 @@ export class MainPageComponent implements OnInit {
   changeStyle() {
     this.cardTable = !this.cardTable;
   }
+  rotate() {
+    this.state = (this.state === 'default' ? 'rotated' : 'default');
+}
 }
