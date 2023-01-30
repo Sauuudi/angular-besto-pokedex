2cfc389bd01883d2c6aedd306a88c1c4cdaffdcf
Sauuudi <iliasbox2@gmail.com>
2023-01-30 18:50:42 +0100

    created search component

---
+++ src/app/search-component/search-component.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { SearchHelperService } from '../shared/helpers/search-helper.service';
+
+@Component({
+  selector: 'app-search-component',
+  templateUrl: './search-component.component.html',
+  styleUrls: ['./search-component.component.scss'],
+})
+export class SearchComponentComponent {
+  @Input() placeholder = '';
+  textSearched = '';
+
+  constructor(private searchHelper: SearchHelperService) {}
+
+  onTyping() {
+    this.searchHelper.emit(this.textSearched.trim());
+  }
+
+  onReset() {
+    this.textSearched = '';
+    this.searchHelper.emit('');
+  }
+}
